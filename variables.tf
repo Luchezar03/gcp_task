c3dfbbd92f8ff393492841f9cace37da0723f4d7
Luchezar03 <57143347+Luchezar03@users.noreply.github.com>
2021-07-04 17:33:21 +0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "network_name" {
+  type    = string
+  default = "simple-gcp"
+}
+
+variable "admin_project" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "target_project_name" {
+  type    = string
+  default = "gcp-project-june-2021"
+}
+
+variable "domain" {
+  type = string
+}
+
+variable "billing_account_id" {
+  type = string
+}
+
+variable "folder_id" {
+  type = string
+}
+
+variable "org_id" {
+  type = string
+}
+
+variable "prefix" {
+  default = "dev"
+}
+
+variable "function_location" {
+}
+
+variable "name" {
+  default = "gke-june-2021"
+}
+
+variable "location" {
+  default = "europe-west2-a"
+}
